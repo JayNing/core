9202fe6eb9db56baf76e8b3685c02a48f8d09e96
JayNing <472420531@qq.com>
2020-03-01 14:47:34 +0800

    测试BeanUtils的克隆方式

--- src/main/java/com/jay/demo/design/prototype/BeanUtilsDemo.java
+++ src/main/java/com/jay/demo/design/prototype/BeanUtilsDemo.java
@@ -17,16 +17,30 @@ public class BeanUtilsDemo {
         deepBook.setDeepAuthor(deepAuthor);
         System.out.println(deepBook);
 
-        DeepBook cloneDeepBook = new DeepBook();
         /**
          * BeanUtils 使用的是浅克隆
          */
+
+        //复制一个原对象赋值给新对象，看看后面浅克隆后，改变克隆的对象的引用属性，对这个赋值的新对象有没有影响
+        DeepBook newDeepBook = deepBook;
+        DeepBook cloneDeepBook = new DeepBook();
 
         BeanUtils.copyProperties(deepBook,cloneDeepBook);
         System.out.println("克隆后的结果：" + cloneDeepBook);
         System.out.println(deepBook == cloneDeepBook);
         System.out.println(deepBook.getDeepAuthor() == cloneDeepBook.getDeepAuthor());
         System.out.println(deepBook.getName() == cloneDeepBook.getName());
+
+        System.out.println("修改克隆后的对象，对原对象的影响-----------：" );
+        DeepAuthor author = cloneDeepBook.getDeepAuthor();
+        author.setName("吴承恩");
+        author.setAge(320);
+        cloneDeepBook.setDeepAuthor(author);
+        System.out.println(deepBook.getDeepAuthor() == cloneDeepBook.getDeepAuthor());
+        System.out.println(cloneDeepBook.getDeepAuthor());
+        System.out.println(deepBook.getDeepAuthor());
+        //查看赋值的新对象的引用属性有没有被改变
+        System.out.println(newDeepBook.getDeepAuthor());
 
     }
 }
